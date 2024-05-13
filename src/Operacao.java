592d60a9d0f68c821a26b2cf956d1c8c12d3a1a6
Rui Rodrigues <ruirr31@proton.me>
2024-05-13 12:00:40 +0100

    Changes by admin

---
+++ src/Operacao.java
@@ -0,0 +1,12 @@
+package exercicios.exercicio_animais.src;
+
+import java.util.Date;
+import exercicios.exercicio_animais.src.Intervencao.*; // Importar todas as classes do pacote Intervencao
+
+public class Operacao {
+    private Date data_inicio;
+    private Date data_fim;
+    private Intervencao intervencao;
+    private double custo;
+    private double duracao;
+}
